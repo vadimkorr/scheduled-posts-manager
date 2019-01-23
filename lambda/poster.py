00801ec94b20caaa430898b24cc0ae7b89687c32
Vadim <korobeinikovv@mail.ru>
2019-01-23 23:18:06 +0300

    Added script for lambda

---
+++ lambda/poster.py
@@ -0,0 +1,13 @@
+import tweepy
+
+consumer_key=''
+consumer_secret=''
+access_token=''
+access_token_secret=''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+api.update_status('Hello from Python')
