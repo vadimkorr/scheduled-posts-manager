489db7b41f9a1e1e872950e9a674c2c3bc25ef23
Vadim <korobeinikovv@mail.ru>
2019-03-29 23:24:07 +0300

    Added new test routes, styles

--- scm/hello.py
+++ scm/hello.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 app = Flask(__name__)
 
+date_time_started = datetime.now()
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def status():
+    status = {'date_time_started': date_time_started}
+    return render_template('home.html', status=status)
+
+
+@app.route('/posts/<id>')
+def get_posts(id):
+    return 'Post id: %s' % id
+
+# @app.route('/posts/new', methods=['POST'])
+# def add_posts():
+#     post = request.model
+#     return 'New post model'
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port='5005', debug=True)
